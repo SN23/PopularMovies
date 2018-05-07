0f11c4aef0a745f799d8bc0189173a8f212921a1
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-05-07 15:51:22 -0400

    created reviewAdapter

---
+++ app/src/main/java/com/sukhjinder/popularmovies/adapter/ReviewAdapter.java
@@ -0,0 +1,66 @@
+package com.sukhjinder.popularmovies.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.sukhjinder.popularmovies.R;
+import com.sukhjinder.popularmovies.model.Review;
+import com.sukhjinder.popularmovies.model.Trailer;
+
+import java.util.ArrayList;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder> {
+
+    private ArrayList<Review> reviews;
+
+    public ReviewAdapter(ArrayList<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewAdapter.ViewHolder holder, int position) {
+        holder.bind(reviews.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+
+    public void addReview(Review review) {
+        reviews.add(review);
+    }
+
+    public void clearAll() {
+        reviews.clear();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView author;
+        private TextView content;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            author = itemView.findViewById(R.id.author);
+            content = itemView.findViewById(R.id.content);
+        }
+
+        public void bind(final Review review) {
+            author.setText(review.getAuthor());
+            content.setText(review.getContent());
+        }
+    }
+}
