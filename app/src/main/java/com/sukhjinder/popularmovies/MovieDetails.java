830092cb24f907d3ed61d0901029a4f6735b3b0f
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-05-12 23:41:05 -0400

    added collapsing toolbar, added db calls

--- app/src/main/java/com/sukhjinder/popularmovies/MovieDetails.java
+++ app/src/main/java/com/sukhjinder/popularmovies/MovieDetails.java
@@ -1,21 +1,31 @@
 package com.sukhjinder.popularmovies;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.sukhjinder.popularmovies.data.MovieContract;
+import com.sukhjinder.popularmovies.data.MovieProvider;
 import com.sukhjinder.popularmovies.adapter.ReviewAdapter;
 import com.sukhjinder.popularmovies.adapter.TrailerAdapter;
 import com.sukhjinder.popularmovies.api.FetchReviews;
@@ -30,9 +40,10 @@ import static android.support.v7.widget.RecyclerView.HORIZONTAL;
 import static android.support.v7.widget.RecyclerView.VERTICAL;
 
 
-public class MovieDetails extends AppCompatActivity {
+public class MovieDetails extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Movie movie;
+    private String movieID;
     private static final String BASE_URL_POSTER = "http://image.tmdb.org/t/p/w342/";
     private static final String BASE_URL_BACKDROP = "http://image.tmdb.org/t/p/w1280/";
     private static String Base_URL_YOUTUBE = "https://www.youtube.com/watch?v=";
@@ -40,18 +51,35 @@ public class MovieDetails extends AppCompatActivity {
     private RecyclerView trailerRecyclerView;
     private TrailerAdapter trailerAdapter;
     private ArrayList<Trailer> trailers;
-    private RecyclerView reviewRecyclerView;
-    private FloatingActionButton favoriteBtn;
 
+    private RecyclerView reviewRecyclerView;
     private ReviewAdapter reviewAdapter;
     private ArrayList<Review> reviews;
+
+    private FloatingActionButton favoriteBtn;
     private boolean onlineStatus;
+
+    MovieProvider movieProvider;
+    private static final int CURSOR_LOADER_ID = 23;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details);
+
+        movie = getIntent().getParcelableExtra("movieDetails");
+        movieID = String.valueOf(movie.getId());
+
+        getSupportLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+
+        movieProvider = new MovieProvider();
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(movie.getTitle());
 
         final Context context = this;
         ImageView moviePoster = findViewById(R.id.movie_details_poster);
@@ -66,8 +94,7 @@ public class MovieDetails extends AppCompatActivity {
         Utils utils = new Utils();
         onlineStatus = utils.isOnline(context);
 
-        movie = getIntent().getParcelableExtra("movieDetails");
-        String movieID = String.valueOf(movie.getId());
+
         trailers = new ArrayList<>();
         reviews = new ArrayList<>();
 
@@ -77,7 +104,7 @@ public class MovieDetails extends AppCompatActivity {
                 context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Base_URL_YOUTUBE + trailer.getKey())));
             }
         });
-        trailerRecyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false));
+        trailerRecyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
         trailerRecyclerView.setAdapter(trailerAdapter);
         DividerItemDecoration trailerItemDecor = new DividerItemDecoration(context, HORIZONTAL);
         trailerRecyclerView.addItemDecoration(trailerItemDecor);
@@ -88,23 +115,24 @@ public class MovieDetails extends AppCompatActivity {
         DividerItemDecoration reviewItemDecor = new DividerItemDecoration(context, VERTICAL);
         reviewRecyclerView.addItemDecoration(reviewItemDecor);
 
-        trailerApiCall(movieID);
-        reviewApiCall(movieID);
+        if (onlineStatus) {
+            trailerApiCall(movieID);
+            reviewApiCall(movieID);
+        } else {
+            Toast.makeText(MovieDetails.this, "No Internet Connectivity", Toast.LENGTH_SHORT).show();
+        }
 
 
-        Picasso.with(context)
+        Picasso.get()
                 .load(BASE_URL_POSTER + movie.getPoster())
-                .fit()
-                .centerCrop()
                 .into(moviePoster);
 
-        Picasso.with(context)
+        Picasso.get()
                 .load(BASE_URL_BACKDROP + movie.getBackdrop())
                 .fit()
                 .centerCrop()
                 .into(movieBackdrop);
 
-        getSupportActionBar().setTitle(movie.getTitle());
         movieYears.setText(movie.getReleaseDate().substring(0, 4));
         movieRating.setText(Double.toString(movie.getUserRating()) + "/10");
         moviePlotSynopsis.setText(movie.getPlotSynopsis());
@@ -112,30 +140,52 @@ public class MovieDetails extends AppCompatActivity {
         favoriteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Added to Favorites", Toast.LENGTH_SHORT).show();
-                Intent favMovieIntent = new Intent(context, Favorites.class);
-                favMovieIntent.putExtra("favMovie", movie);
-                context.startActivity(favMovieIntent);
+                if (addToFavorites(movie.getId(), movie.getTitle(), movie.getPoster()) == null) {
+                    Toast.makeText(context, "Already in Favorites", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Added to Favorites", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void trailerApiCall(String movieID) {
-        if (onlineStatus) {
-            trailerAdapter.clearAll();
-            new FetchTrailers(trailerAdapter).execute(movieID);
-        } else if (onlineStatus) {
-            Toast.makeText(MovieDetails.this, "No Internet Connectivity", Toast.LENGTH_SHORT).show();
-        }
+        trailerAdapter.clearAll();
+        new FetchTrailers(trailerAdapter).execute(movieID);
+
     }
 
     private void reviewApiCall(String movieID) {
-        if (onlineStatus) {
-            reviewAdapter.clearAll();
-            new FetchReviews(reviewAdapter).execute(movieID);
-        } else if (onlineStatus) {
-            Toast.makeText(MovieDetails.this, "No Internet Connectivity", Toast.LENGTH_SHORT).show();
-        }
+        reviewAdapter.clearAll();
+        new FetchReviews(reviewAdapter).execute(movieID);
+
     }
 
+    private Uri addToFavorites(int id, String title, String poster) {
+        ContentValues cv = new ContentValues();
+        cv.put(MovieContract.MovieEntry.COLUMN_ID, id);
+        cv.put(MovieContract.MovieEntry.COLUMN_TITLE, title);
+        cv.put(MovieContract.MovieEntry.COLUMN_POSTER, poster);
+        return getContentResolver().insert(MovieContract.MovieEntry.CONTENT_URI, cv);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this,
+                MovieContract.MovieEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
