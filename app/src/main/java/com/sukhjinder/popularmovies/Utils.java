da9ec809dc3fc4fbd072f81a27d1d76e11c55984
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-05-15 14:03:03 -0400

    changed method to static

--- app/src/main/java/com/sukhjinder/popularmovies/Utils.java
+++ app/src/main/java/com/sukhjinder/popularmovies/Utils.java
@@ -11,7 +11,7 @@ public class Utils {
     // Network Connectivity Code from Stackoverflow
     // https://stackoverflow.com/questions/1560788/how-to-check-internet-access-on-android-inetaddress-never-times-out
 
-    public boolean isOnline(Context context) {
+    public static boolean isOnline(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = Objects.requireNonNull(connectivityManager).getActiveNetworkInfo();
         return networkInfo != null && networkInfo.isConnectedOrConnecting();
